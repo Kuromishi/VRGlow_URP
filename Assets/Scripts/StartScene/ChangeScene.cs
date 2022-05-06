12e0cc7f147898eeaf1a7de857b933b7f770a02c
ClaireMilk <79968107+ClaireMilk@users.noreply.github.com>
2022-05-06 11:58:33 +0800

    tu小改

--- Assets/Scripts/StartScene/ChangeScene.cs
+++ Assets/Scripts/StartScene/ChangeScene.cs
@@ -8,20 +8,24 @@ public class ChangeScene : MonoBehaviour
     public void TutorialScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        AkSoundEngine.StopAll();
     }
 
     public void GameScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        AkSoundEngine.StopAll();
     }
 
     public void RestartTutorial()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        AkSoundEngine.StopAll();
     }
 
-    public void ReturnTitle()
+    public void EnterGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        AkSoundEngine.StopAll();
     }
 }
